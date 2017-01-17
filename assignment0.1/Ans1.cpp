7496e8dee13a01f06c4fde4f420c72e94bc6928d
sumir <2015KUCP1017@iiitkota.ac.in>
2017-01-17 21:27:34 +0530

    assignment 0.1 done

--- assignment0.1/Ans1.cpp
+++ assignment0.1/Ans1.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 #include<fstream>
-#include<strings.h>
+#include<math.h>
+#include<ctype.h>
+#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
 
 using namespace std;
+
+int makeNum(string s)
+{
+	unsigned int num=0,count=0;
+	while(count<s.size())
+	{
+		if(isdigit(s[count]))
+			num = num + (s[count]-48)*(int)(pow(10,s.size()-count-1));
+		else
+			return -1;  	
+		
+		count++;	
+	}
+	return num;
+}
 
 int main(int argc,char *argv[])
 {
@@ -65,9 +84,133 @@ int main(int argc,char *argv[])
 	}
 	
 	else if(argc == 3)
-	{
+	{	
+
+		int readRate = makeNum(argv[1]);
+		int outRate = makeNum(argv[2]);				
+
+		if(readRate == -1 || outRate == -1)
+			{
+				cout<<"Entered value is not an integer\n";
+				return 0;		
+			}	
+		while(1)
+		{
+			
+			ifstream fp;
+			string s1;
+			string remove,name;
+			float x=0,userMode=0,systemMode=0,idleMode=0,totalTime=0,memTotal=0,memFree=0,avgMem=0,processCreateRate=0,contextSwitRate=0,processDiff=0,contextSwitDiff=0;
+			float reads=0,writes=0;				
+			int time=0,k=0,i=0;
+			
+			int processes[100]={0},contextSwitches[100]={0};
+			
+			fp.open("/proc/meminfo",ios::in);
+			fp>>remove;	//remove the name from the stream
+			fp>>memTotal;
+			fp.close();	
 						
-	}
+			while(time<outRate)
+			{
+				//Percentage of time in idle, system, and user Mode also the context switches and rate of process creation			
+				fp.open("/proc/stat",ios::in);
+				fp>>remove; //remove the name cpu from the stream			
+				fp>>x;
+				userMode += x;
+				totalTime += x;
+
+				fp>>x;totalTime += x;
+				fp>>x;totalTime += x;
+			
+				systemMode+=x;
+				fp>>x;
+				idleMode+=x;
+				totalTime += x;
+			
+				fp>>x;totalTime += x;
+				fp>>x;totalTime += x;
+				fp>>x;totalTime += x;
+				fp>>x;totalTime += x;
+				fp>>x;totalTime += x;
+				fp>>x;totalTime += x;					
+				
+				getline(fp,remove);
+				getline(fp,remove);				
+				getline(fp,remove);
+				getline(fp,remove);
+				getline(fp,remove);
+				getline(fp,remove);
+				
+				fp>>remove; //remove the word 'ctxt'
+				
+				fp>>contextSwitches[k];
+
+				getline(fp,remove); //remove two lines from the stream
+				getline(fp,remove);
+			
+				fp>>remove; //remove the word 'processes'
+
+				fp>>processes[k]; 
+
+				fp.close();
+				
+				//amount and percentage of free memeory in system
+				fp.open("/proc/meminfo",ios::in);
+				
+				getline(fp,remove);	//remove the first line from the stream
+				fp>>remove;		//remove the name MemFree from the stream
+				fp>>x;
+				memFree+=x;
+
+				fp.close();
+				
+				//Rate of disk read and write in the system
+				fp.open("/proc/diskstats",ios::in);				
+				while(!fp.eof())
+				{
+					fp>>remove>>remove>>name; //remove the first 2 values from the stream
+					if(name == "sda")
+						break;	
+					else
+						getline(fp,remove);	// if the not sda the remove the whole line from the stream
+				}	
+		
+				fp>>x;
+				reads += x;
+				fp>>x>>x>>x>>x;
+				writes += x;
+				fp.close();
+
+				k++;				
+				sleep(readRate);							
+				time+=readRate;
+		
+			}
+			cout<<"\n";
+			avgMem = memFree/(outRate/readRate);
+			for(i=1;i<k;i++)
+			{
+				contextSwitDiff += contextSwitches[i] - contextSwitches[i-1];
+				processDiff += processes[i] - processes[i-1];
+		
+			}
+				
+			processCreateRate = processDiff/(outRate/readRate);
+			contextSwitRate = contextSwitDiff/(outRate/readRate);
+
+			cout<<"The percentage of time the computer is in User Mode: "<<(userMode/totalTime)*100<<"%"<<endl;						
+			cout<<"The percentage of time the computer is in System Mode: "<<(systemMode/totalTime)*100<<"%"<<endl;						
+			cout<<"The percentage of time the computer is in Idle Mode: "<<(idleMode/totalTime)*100<<"%"<<endl;			
+			printf("The amount of average free memory in the system:  %.1f kB\n",avgMem);
+			cout<<"The percentage of free memory in the system:  "<<(avgMem/memTotal)*100<<"%"<<endl;
+			printf("The rate of disk read is: %0.1f blocks\n",(reads/(outRate/readRate)));			
+			printf("The rate of disk write is: %0.1f blocks	\n",(writes/(outRate/readRate)));				
+			printf("The rate of context switches in kernel: %.1f \n",contextSwitRate);
+			printf("The rate of process creation is: %f\n",processCreateRate);	
+				
+		}			
+	}	
 
  return 0;
 }
