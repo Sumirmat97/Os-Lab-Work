195e638606f75542507d47db51aa9697fe8e8ab5
sumir <2015KUCP1017@iiitkota.ac.in>
2017-01-24 00:12:24 +0530

    80% done

--- lab2/Ans1.c
+++ lab2/Ans1.c
@@ -1,17 +1,139 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+
+void removeEndline(char *str)
+{
+	int i;
+	for(i=0;i<strlen(str);i++)
+	{
+		if(str[i] == '\n')
+			str[i] = '\0';
+	}
+}
+
+void execute(char *command[],int flag)
+{
+
+	int waitValue;
+
+	if(!strcmp(command[0],"cd"))	//if cd command is given in input
+	{
+		if(command[1] != NULL)
+		{
+			if(chdir(command[1]) == -1) // if directory does not exists
+			{
+				printf("No such directory found\n");
+			}
+		}
+		else			//if no path is given after the cd command change directory to home
+		{
+			chdir("/home");	
+		}
+		return;
+
+	}
+	
+	pid_t pid = fork();
+	if(pid < 0)
+	{
+		printf("*** ERROR: forking child process failed\n");	//if fork() fails
+		return;
+	}
+	else if(!pid)
+	{		
+		//for child process
+		if(execvp(command[0],command) < 0)  
+		{
+			printf("command not found\n");
+		}
+		exit(0);
+	}
+	else
+	{	//for parent process
+		if(flag == 0)	//Wait only if the process is foreground 
+		{	
+			waitpid(pid,NULL,0);
+		}
+		else
+		{
+			printf("%d\n",pid);
+		}
+	}	
+}
 
 int main()
 {
-  char input[100];
-  do
-  {
-    printf("Name/computer$ ");
-    fgets(input,100, stdin);
-    if(strcmp(input,"\n")) //print the input only if not \n
-      printf("%s\n",input);
-  }while(strcmp(input,"exit"));
+	char *input = NULL;
+	char *token;
+	size_t size;
+	int counter=0,flag=0,i; //flag 0. => foreground, 1. => background
+	char *path = NULL;			
+	char *args[10];
+	
+	do
+	{
+		
+		for(i=0;i<10;i++)		//initialising each of the strings to null
+			args[i] = (char *)NULL;
+	
+		printf("Sumir@ListenerShell:%s$ ",getcwd(path,size));
 
-  return 0;  
+		getline(&input,&size,stdin);
+		
+		if(feof(stdin)) break;	//for ctrl+D 
+		if(!strcmp(input,"exit\n")) break; //if exit is given in input
+		if(!strcmp(input,"\n")) continue; //if '\n' is given in input
+
+		removeEndline(input);
+
+		token = strtok(input," ");
+		
+		while(token!=NULL)
+		{ 
+			args[counter++] = token;			
+			if(!strcmp(token,"&"))
+			{	
+				flag = 1;				//if & is found put null in args and execute the command till there
+				args[--counter] = (char *)NULL;	
+				printf("%d ",flag);	
+				execute(args,flag);
+				
+				for(i=0;i<=counter;i++)			//re initialise the array and the counter
+					args[i] = (char *)NULL;
+				counter=0;
+				flag = 0;
+				printf("%d  ",flag);				
+			}	
+			else if(!strcmp(token,"&&"))			//if && comes execute the commands differently. 
+			{	
+				flag = 0;			
+				args[--counter] = (char *)NULL;		
+				execute(args,flag);
+				
+				for(i=0;i<=counter;i++)			//re initialise the array and the counter
+					args[i] = (char *)NULL;	
+				counter = 0;		
+			}
+			token = strtok(NULL," ");
+					
+		}	
+		
+		args[counter] = (char *)NULL;
+		flag = 0;
+		if(counter > 0)						//check if args has non null value in the first index itself 
+			execute(args,flag);			
+
+		counter = 0;	
+		input = NULL;
+		flag = 0;
+	
+	}while(1);
+	
+	printf("\n**Exiting ListenerShell**\n");
+	
+	return 0;  
 }
--- lab2/ReadMe.txt
+++ lab2/ReadMe.txt
@@ -1,1 +1,11 @@
 This is the submission of lab 2 assignment
+
+The commands that work are gedit ls mkdir man netstat gcc top strace exit ctrl+D clear nano pwd cat ps clear
+ 
+The processes can be run in both foreground and background. If a process is run in background using & the pid of the child is displayed on the shell. 
+
+It can also execute multiple commands in background	eg. ls & gedit file.txt &
+both these will run in the background just like the do in the shell provided by ubuntu.
+
+It can also execute multiple commands using '&&'  eg. ls && cd .. && gedit file.txt
+All of these commands will be executed one by one. 
